b2d4ef95220993ecba36d2e7d9bd53f027337eb6
Janne Marie <janne_m_tvetene@hotmail.com>
2025-05-25 23:19:39 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 var typed = new Typed('.aboutMe', {
-    strings: ['Janne Marie', 'a backend developer', '', 'always working to be the best backend developer'],
+    strings: [
+  'Janne Marie',
+  'a developer enthusiast',
+  'a cybersecurity student',
+  'building secure systems',
+  'curious about how things break (and how to fix them)',
+],
     smartBackspace: true,
     typeSpeed: 100,
     backSpeed: 100,
